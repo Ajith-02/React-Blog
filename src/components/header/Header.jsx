1ba70ae8e44ca6454d6827b5edc397bb005c4e96
Ajith-02 <89974825+Ajith-02@users.noreply.github.com>
2022-04-26 16:11:35 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleSm">Happy Days</span>
         <span className="headerTitleLg">Blog</span>
       </div>
       <img className="headerImg" src="https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77700359247.jpg" alt="headerImg" />
